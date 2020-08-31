c034b3e91843bff71bf6ad31461cd8b59943645c
Luis Valgoi <luis.valgoi@sap.com>
2020-08-31 14:57:54 -0300

    refactor: test classes and added testprovider

--- src/components/Layout/CenteredContent.test.js
+++ src/components/Layout/CenteredContent.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render } from '../../util/TestProvider';
 
 import CenteredContent from './CenteredContent';
 
---
+++ src/util/TestProvider.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+const AllTheProviders = ({ children }) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+
+export { customRender as render }
