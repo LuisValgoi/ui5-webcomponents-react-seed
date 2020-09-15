0060e238328bba7a50ebd1d96243684fc70745a6
Isaque Hoffmeister <isaque.ha@gmail.com>
2020-09-14 23:45:10 -0300

    feat: sort dialog footer

--- src/components/Sort/SortDialog.js
+++ src/components/Sort/SortDialog.js
@@ -1,22 +1,109 @@
-import React, { useEffect, useRef } from 'react';
-import { Dialog, List, StandardListItem } from '@ui5/webcomponents-react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, ButtonDesign, Dialog, FlexBox, FlexBoxJustifyContent, List, StandardListItem } from '@ui5/webcomponents-react';
+import { spacing } from '@ui5/webcomponents-react-base';
+import { useTranslation } from 'react-i18next';
 
 export default function SortDialog({open}) {
   const dialogRef = useRef(null);
   const listData = ["1", "3", "abc"];
+  const [openDialog, setOpenDialog] = useState(false);
+  const { t } = useTranslation();
 
   useEffect(() => {
-    open ? dialogRef.current.open() : dialogRef.current.close();
+    setOpenDialog(open);
   }, [open]);
+
+  useEffect(() => {
+    openDialog ? dialogRef.current.open() : dialogRef.current.close();
+  }, [openDialog]);
+
+  const onCloseSortDialog = () => {
+    setOpenDialog(false);
+    return;
+  }
+
+  const onApplySort = () => {
+    setOpenDialog(false);
+    return;
+  }
+
+  const getFooter = () => {
+    return (
+      <FlexBox
+        justifyContent={FlexBoxJustifyContent.End}
+        style={{ ...spacing.sapUiTinyMargin, width: '100%' }}
+      >
+        <Button
+          design={ButtonDesign.Transparent}
+          onClick={onCloseSortDialog}
+        >
+          {t('sort.dialog.close')}
+        </Button>
+        <Button
+          design="Emphasized"
+          onClick={onApplySort}
+        >
+          {t('sort.dialog.apply')}
+        </Button>
+      </FlexBox>
+    );
+  };
 
   return (
     <Dialog
-      ref={dialogRef}>
+      ref={dialogRef}
+      headerText={t('sort.dialog.title')}
+      footer={getFooter()}>
       <List>
         {listData.map((item) => (
           <StandardListItem key={item} info={item}>{item}</StandardListItem>
         ))}
       </List>
+
+      {/* <div
+        style={{
+          width: '15rem',
+          maxWidth: '100%',
+          padding: '1rem'
+        }}
+      >
+        <FlexBoxColumnWrapper style={{ width: '100%' }}>
+          <TitleWrapper level={TitleLevel.H4} style={{ fontWeight: 'bold' }}>
+            {t('sortDialog.sortDirection')}
+          </TitleWrapper>
+          {SortDialogModel.getOrders().map((orderProperty, index) => (
+            <LabelAndRadioButtonWrapper
+              key={`OrderGroup${orderProperty}`}
+              name='OrderGroup'
+              value={orderProperty}
+              checked={orderProperty === order}
+              onChange={e => setOrder(e.target.value)}
+              text={SortDialogModel.getOrdersText()[index]} />
+          ))}
+        </FlexBoxColumnWrapper>
+
+        <FlexBoxColumnWrapper style={{ width: '100%' }}>
+          <TitleWrapper
+            level={TitleLevel.H4}
+            style={{
+              fontWeight: 'bold',
+              marginTop: '1rem'
+            }}
+          >
+            {t('sortDialog.sortBy')}
+          </TitleWrapper>
+          {sortProperties.map((sortProperty, index) => (
+            <LabelAndRadioButtonWrapper
+              key={`PropertiesGroup${sortProperty}`}
+              name='PropertiesGroup'
+              value={sortProperty}
+              checked={sortProperty === property}
+              onChange={e => setProperty(e.target.value)}
+              text={sortText[index]} />
+          ))}
+        </FlexBoxColumnWrapper>
+      </div> */}
+
     </Dialog>
   );
 }
--- src/components/Sort/SortList.js
+++ src/components/Sort/SortList.js
@@ -1,19 +1,22 @@
 import { Button, List, StandardListItem } from '@ui5/webcomponents-react';
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import SortDialog from './SortDialog';
 
 export default function SortList() {
   const listData = ["1", "3", "abc"];
   const [ openSort, setOpenSort ] = useState(false);
+  const { t } = useTranslation();
 
   const handleOpenDialog = () => {
     setOpenSort(false);
     setOpenSort(true);
+    return;
   }
 
   return (
     <div>
-      <Button onClick={() => handleOpenDialog()}>Sort</Button>
+      <Button onClick={handleOpenDialog}>{t('sort.button.title')}</Button>
       <SortDialog open={openSort} />
       <List>
         {listData.map((item) => (
