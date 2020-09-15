c1dfa618f059778c4a57d0b9ab78eef56341f65e
Isaque Hoffmeister <isaque.ha@gmail.com>
2020-09-14 22:15:10 -0300

    feat: start sort structure

---
+++ src/components/Sort/SortButton.js
@@ -0,0 +1,12 @@
+import { Button } from '@ui5/webcomponents-react'
+import React from 'react'
+
+export default function SortButton() {
+  return (
+    <>
+      <Button>
+        SORT
+      </Button>
+    </>
+  )
+}
--- src/pages/Todo/List/TodoList.js
+++ src/pages/Todo/List/TodoList.js
@@ -5,9 +5,12 @@ import { Helmet } from 'react-helmet';
 import HyperLink from '../../../components/HyperLink/HyperLink';
 import BrowserURL from '../../../util/BrowserURL';
 import ComponentValidator from '../../../auth/Components/Validator';
+import { List, StandardListItem } from '@ui5/webcomponents-react';
+import SortButton from '../../../components/Sort/SortButton';
 
 export default function TodoList() {
   const history = useHistory();
+  const listData = ["1", "3", "abc"];
 
   return (
     <>
@@ -26,6 +29,14 @@ export default function TodoList() {
         <HyperLink
           text='Drop Application (this is a restricted text and you should not see unless you have access)' />
       </ComponentValidator>
+      <div>
+        <SortButton/>
+        <List>
+          {listData.map((item) => (
+            <StandardListItem key={item} info={item}>{item}</StandardListItem>
+          ))}
+        </List>
+      </div>
     </>
   )
 }
