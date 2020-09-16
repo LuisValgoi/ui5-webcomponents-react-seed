9dd3bec3ba7c4907af56b7873d6ff09d2f8fdfcb
Luis Pohren <luis.pohren@sap.com>
2020-09-16 14:44:26 -0300

    add sample link to information dialog

--- src/pages/Todo/List/TodoList.js
+++ src/pages/Todo/List/TodoList.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useRef} from 'react'
 import { useHistory } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import HyperLink from '../../../components/HyperLink/HyperLink';
 import BrowserURL from '../../../util/BrowserURL';
 import ComponentValidator from '../../../auth/Components/Validator';
+import InformationDialog, { Type } from '../../../components/InformationDialog/InformationDialog'
 
 export default function TodoList() {
+  const dialogRef = useRef(null);
   const history = useHistory();
+  const openInformationDialog = () => {
+    dialogRef.current.open()
+  };
 
   return (
     <>
@@ -19,6 +24,17 @@ export default function TodoList() {
       <ComponentValidator allowedAuthorities={['canAccessDropApplication']} authorityKey="permissions">
         <HyperLink text="Drop Application (this is a restricted text and you should not see unless you have access)" />
       </ComponentValidator>
+      <br />
+      <HyperLink
+        onClick={openInformationDialog}
+        text='Open Information Dialog' />
+      <InformationDialog
+        avoidEscapeClose
+        dialogRef={dialogRef}
+        type={Type.Warning}
+        headerText={'Header text'}
+        closeButtonText={'Close'}
+        innerText={'Inner text'} />
     </>
   );
 }
