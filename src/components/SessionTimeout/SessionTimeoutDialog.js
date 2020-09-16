4b0beac39404ff2a5d69069b57952bc29465be91
Isaque Hoffmeister <isaque.ha@gmail.com>
2020-09-16 18:38:10 -0300

    fix: tests for session timeout

--- src/components/SessionTimeout/SessionTimeoutDialog.js
+++ src/components/SessionTimeout/SessionTimeoutDialog.js
@@ -18,17 +18,17 @@ const TIMEOUT_MODE = {
 
 const WARNING_MODE = {
   type: Type.Warning,
-  headerText: i18n.t('session.warning.expired'),
+  headerText: i18n.t('session.warning'),
   closeButtonText: i18n.t('app.generics.close'),
-  innerText: i18n.t('session.warning.expired.text'),
+  innerText: i18n.t('session.warning.text'),
   onClose: null,
 };
 
-const SessionTimeoutDialog = ({ hasExpiredLimit = SESSION.REFRESH_LIMIT, isExpiringLimit = SESSION.REFRESH_WARNING }) => {
+const SessionTimeoutDialog = ({ timeoutScale = SESSION.TIMEOUT_INTERVAL, hasExpiredLimit = SESSION.REFRESH_LIMIT, isExpiringLimit = SESSION.REFRESH_WARNING }) => {
   const dialogRef = useRef(null);
   const ACTIVITY_EVENTS = ['click', 'focus', 'blur', 'keyup', 'keydown', 'mousemove', 'scroll'];
   const [sessionIntervalCount, setSessionIntervalCount] = useState(1);
-  const [options, setOptions] = useState(TIMEOUT_MODE);
+  const [options, setOptions] = useState(WARNING_MODE);
   let sessionIntervalFinder = null;
 
   useEffect(() => {
@@ -39,14 +39,14 @@ const SessionTimeoutDialog = ({ hasExpiredLimit = SESSION.REFRESH_LIMIT, isExpir
     sessionIntervalFinder = setInterval(() => {
       if (sessionIntervalCount >= isExpiringLimit && sessionIntervalCount < hasExpiredLimit) {
         setOptions(WARNING_MODE);
-        dialogRef.current.open();
+        dialogRef.current && dialogRef.current.open();
       } else if (sessionIntervalCount >= hasExpiredLimit) {
         setOptions(TIMEOUT_MODE);
-        dialogRef.current.open();
+        dialogRef.current && dialogRef.current.open();
       }
 
       setSessionIntervalCount(sessionIntervalCount + 1);
-    }, SESSION.TIMEOUT_INTERVAL);
+    }, timeoutScale);
   });
 
   const handleUserActivity = () => {
--- src/components/SessionTimeout/SessionTimeoutDialog.test.js
+++ src/components/SessionTimeout/SessionTimeoutDialog.test.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '../../util/TestSetup';
+import { render, screen, waitFor } from '../../util/TestSetup';
 
 import SessionTimeoutDialog from './SessionTimeoutDialog';
 
 describe('SessionTimeoutDialog.js Test Suite', () => {
-  beforeAll(() => {
-    render(<SessionTimeoutDialog />);
+  beforeEach(() => {
+    render(<SessionTimeoutDialog timeoutScale={1} hasExpiredLimit={15} isExpiringLimit={13} />);
   });
 
   test('should match snapshot', () => {
     const { asFragment } = render(<SessionTimeoutDialog />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('should render, wait 13 cycles and see the text “Session Almost Expiring”', async () => {
+    const text = 'Session Almost Expiring';
+    const warning = await waitFor(() => screen.getByText(text));
+    expect(warning).toBeInTheDocument();
+  });
+
+  test('should render, wait 15 cycles and see the text “Session Expired”', async () => {
+    const text = 'Session Expired';
+    const expire = await waitFor(() => screen.getByText(text));
+    expect(expire).toBeInTheDocument();
   });
 });
