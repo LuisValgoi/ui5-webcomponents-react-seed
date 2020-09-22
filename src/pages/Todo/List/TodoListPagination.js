a0faa3581c490e09743eff46e30d4c2cfba84ac5
Luis Valgoi <luis.valgoi@sap.com>
2020-09-22 18:58:08 -0300

    feat: added first version of pagination

--- src/pages/Todo/List/TodoListPagination.js
+++ src/pages/Todo/List/TodoListPagination.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import { useGet } from '../../../hooks/useRequest';
+import React, { useState } from 'react';
+import { usePaginatedGet } from '../../../hooks/useRequest';
 
 import { List } from '@ui5/webcomponents-react/lib/List';
 import { StandardListItem } from '@ui5/webcomponents-react/lib/StandardListItem';
 import { Spinner } from '@ui5/webcomponents-react';
 import Constants from '../../../util/Constants';
+import { Pagination } from '../../../components/ViewSetting/Pagination';
 
 export default function TodoListPagination() {
-  const { data, status } = useGet(Constants.REACT_QUERY.KEYS.RQ_GET_TODO_LIST, 'GET_TODO_LIST', null);
+  const [page, setPage] = useState(0);
+  const { resolvedData, status } = usePaginatedGet(Constants.REACT_QUERY.KEYS.RQ_GET_TODO_LIST, page, 'GET_TODO_LIST');
+
   return (
     <div>
       {status === Constants.REACT_QUERY.CODES.LOADING ? (
         <Spinner />
       ) : (
-        <List
-          busy={false}
-          footerText={undefined}
-          header={null}
-          headerText={undefined}
-          infiniteScroll={false}
-          inset={false}
-          mode="None"
-          noDataText={undefined}
-          onItemClick={function noRefCheck() {}}
-          onItemDelete={function noRefCheck() {}}
-          onLoadMore={function noRefCheck() {}}
-          onSelectionChange={function noRefCheck() {}}
-          separators="All"
-        >
-          {data.data.todos.map((todo, index) => (
-            <StandardListItem key={index} iconEnd={false} info={todo.number} infoState="None" selected={false}>
-              {todo.name}
-            </StandardListItem>
-          ))}
-        </List>
+        <>
+          <List
+            busy={false}
+            footerText={undefined}
+            header={null}
+            headerText={undefined}
+            infiniteScroll={false}
+            inset={false}
+            mode="None"
+            noDataText={undefined}
+            onItemClick={function noRefCheck() {}}
+            onItemDelete={function noRefCheck() {}}
+            onLoadMore={function noRefCheck() {}}
+            onSelectionChange={function noRefCheck() {}}
+            separators="All"
+          >
+            {resolvedData.content.map((todo, index) => (
+              <StandardListItem key={index} iconEnd={false} info={todo.number} infoState="None" selected={false}>
+                {todo.name}
+              </StandardListItem>
+            ))}
+          </List>
+          <Pagination numberOfElements={resolvedData.numberOfElements} totalPages={resolvedData.totalPages} selectedPage={resolvedData.number} setPage={(params) => setPage(params)} />
+        </>
       )}
     </div>
   );
