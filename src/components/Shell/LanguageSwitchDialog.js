70f388041c7ac87c1f9fa314170d0daf34a731e9
Luis Henrique Pohren <ikepohren@gmail.com>
2020-10-01 11:01:10 -0300

    feat: translate theme switch

--- src/components/Shell/LanguageSwitchDialog.js
+++ src/components/Shell/LanguageSwitchDialog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { Option  } from '@ui5/webcomponents-react/lib/Option';
 import { Select } from '@ui5/webcomponents-react/lib/Select';
@@ -6,13 +7,14 @@ import { Dialog } from '@ui5/webcomponents-react/lib/Dialog';
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 
 const LanguageSwitchDialog = ({dialogRef}) => {
+  const { t } = useTranslation();
   const onChange = (event) => {
     setTheme(event.detail.selectedOption.dataset.value);
   };
   const themeOptions = [
-    { value: 'sap_fiori_3', title: 'Default' },
-    { value: 'sap_belize_hcb', title: 'High Contrast Black' },    
-    { value: 'sap_belize_hcw', title: 'High Contrast White' },
+    { value: 'sap_fiori_3', title: t('shell.button.user.settings.item.themeSwitch.option.default') },
+    { value: 'sap_belize_hcb', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastBlack') },    
+    { value: 'sap_belize_hcw', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastWhite') },
   ];  
 
   return (
