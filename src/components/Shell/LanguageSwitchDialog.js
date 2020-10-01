cd6362d0fd8e1dfb0e362d811d80e33a52f22357
Luis Henrique Pohren <ikepohren@gmail.com>
2020-10-01 14:50:25 -0300

    feat: user information dialog

--- src/components/Shell/LanguageSwitchDialog.js
+++ src/components/Shell/LanguageSwitchDialog.js
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 
 import { Option  } from '@ui5/webcomponents-react/lib/Option';
 import { Select } from '@ui5/webcomponents-react/lib/Select';
-import { Dialog } from '@ui5/webcomponents-react/lib/Dialog';
-import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
+import InformationDialog from '../InformationDialog/InformationDialog';
 
 const LanguageSwitchDialog = ({dialogRef}) => {
   const { t } = useTranslation();
@@ -16,10 +16,15 @@ const LanguageSwitchDialog = ({dialogRef}) => {
     { value: 'sap_belize_hcb', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastBlack') },    
     { value: 'sap_belize_hcw', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastWhite') },
   ];  
+  const style = {
+    select: {
+      width: '100%'
+    }
+  };
 
   return (
-    <Dialog ref={dialogRef}>
-      <Select onChange={onChange}>
+    <InformationDialog dialogRef={dialogRef} headerText={t("shell.button.user.settings.item.themeSwitch.title")}>
+      <Select onChange={onChange} style={style.select}>
         {themeOptions && themeOptions.map(option => {
           return (
             <Option key={option.value} data-value={option.value}>
@@ -28,7 +33,7 @@ const LanguageSwitchDialog = ({dialogRef}) => {
           )
         })}
       </Select>
-    </Dialog>
+    </InformationDialog>
   );
 };
 
