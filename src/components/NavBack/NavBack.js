85778571336d7339a6a95bfeada5bf060b43b6f6
Luis Valgoi <luis.valgoi@sap.com>
2020-10-02 16:10:45 -0300

    refactor: navback default args

--- src/components/NavBack/NavBack.js
+++ src/components/NavBack/NavBack.js
@@ -4,12 +4,12 @@ import { Button, ButtonDesign, FlexBox } from '@ui5/webcomponents-react';
 import { useHistory } from 'react-router-dom';
 import { spacing } from '@ui5/webcomponents-react-base';
 
-export default function NavBack({ text = 'Back' }) {
+export default function NavBack({ text = 'Back', icon = 'nav-back', design = ButtonDesign.Transparent, disabled = false }) {
   const history = useHistory();
 
   return (
     <FlexBox style={spacing.sapUiTinyMargin}>
-      <Button data-testid="navback-wrapper" design={ButtonDesign.Transparent} disabled={false} icon="nav-back" iconEnd={false} onClick={() => history.goBack()} submits={false}>
+      <Button data-testid="navback-wrapper" design={design} disabled={disabled} icon={icon} iconEnd={false} onClick={() => history.goBack()} submits={false}>
         {text}
       </Button>
     </FlexBox>
