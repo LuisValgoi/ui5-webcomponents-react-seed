1abd874311dbc24528898f3edcac618c2400364a
Luis Henrique Pohren <ikepohren@gmail.com>
2020-10-13 15:03:16 -0300

    feat: move consts outside component

--- src/components/ThemeSwitch/ThemeSwitch.js
+++ src/components/ThemeSwitch/ThemeSwitch.js
@@ -6,26 +6,26 @@ import { Select } from '@ui5/webcomponents-react/lib/Select';
 import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
 import InformationDialog from '../InformationDialog/InformationDialog';
 
+const themeOptions = [
+  { value: 'sap_fiori_3', title: 'shell.button.user.settings.item.themeSwitch.option.default' },
+  { value: 'sap_fiori_3_dark', title: 'shell.button.user.settings.item.themeSwitch.option.dark' },
+  { value: 'sap_belize', title: 'shell.button.user.settings.item.themeSwitch.option.belize' },
+  { value: 'sap_fiori_3_hcb', title: 'shell.button.user.settings.item.themeSwitch.option.highContrastBlack' },
+  { value: 'sap_fiori_3_hcw', title: 'shell.button.user.settings.item.themeSwitch.option.highContrastWhite' },
+];
+const style = {
+  select: {
+    width: '100%',
+  },
+};
+
 const ThemeSwitch = ({ dialogRef, storedTheme = localStorage.getItem('reactSeedSelectedTheme') }) => {
   const { t } = useTranslation();
   setTheme(storedTheme ? storedTheme : 'sap_fiori_3');
-
   const onChange = (event) => {
     storedTheme = event.detail.selectedOption.dataset.value;
     localStorage.setItem('reactSeedSelectedTheme', storedTheme);
     setTheme(storedTheme);
-  };
-  const themeOptions = [
-    { value: 'sap_fiori_3', title: t('shell.button.user.settings.item.themeSwitch.option.default') },
-    { value: 'sap_fiori_3_dark', title: t('shell.button.user.settings.item.themeSwitch.option.dark') },
-    { value: 'sap_belize', title: t('shell.button.user.settings.item.themeSwitch.option.belize') },
-    { value: 'sap_fiori_3_hcb', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastBlack') },
-    { value: 'sap_fiori_3_hcw', title: t('shell.button.user.settings.item.themeSwitch.option.highContrastWhite') },
-  ];
-  const style = {
-    select: {
-      width: '100%',
-    },
   };
 
   return (
@@ -35,7 +35,7 @@ const ThemeSwitch = ({ dialogRef, storedTheme = localStorage.getItem('reactSeedS
           themeOptions.map((option) => {
             return (
               <Option key={option.value} data-value={option.value} selected={option.value === storedTheme}>
-                {option.title}
+                {t(option.title)}
               </Option>
             );
           })}
